8a130ce79ae7581b37f1539e10c866965160966c
Galina Karateeva <karateewa.galina@yandex.ru>
2021-04-08 17:26:50 +0200

    - wrapping content in a <main> instead of div - adjustment of accessibility issues - adding figure element instead of div - some visual adjustments

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ const App = () => {
   const [quotes, setQuotes] = useState([]);
 
   useEffect(() => {
-    fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
-      .then(response => response.json())
-      .then(data => data.quotes)
-      .then(quotes => setQuotes(quotes));
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((response) => response.json())
+      .then((data) => data.quotes)
+      .then((quotes) => setQuotes(quotes));
   }, []);
 
   function getNewQuote() {
@@ -26,29 +28,33 @@ const App = () => {
   }
 
   return (
-    <div className="d-flex justify-content-center align-items-center h-100">
-      <div id="main" className="border p-3">
-        <h1 className="text-info text-center" id="heading">
+    <main className="d-flex justify-content-center align-items-center h-100">
+      <div id="main" className="border border-secondary rounded p-3">
+        <h1 className="text-primary text-center" id="heading">
           Here is your daily quote:
         </h1>
         <div id="quote-box" className="text-center">
-          <Quote currentQuote={currentQuote}/>
+          <Quote currentQuote={currentQuote} />
           <div className="row">
             <div className="col-md-1 button">
               <a
-                className="btn btn-info"
+                className="btn btn-primary"
                 id="tweet-quote"
-                title="Twit it!"
                 target="_blank"
-                href={"https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + currentQuote.quote + '" ' + currentQuote.author)}
+                href={
+                  "https://twitter.com/intent/tweet?text=" +
+                  encodeURIComponent(
+                    '"' + currentQuote.quote + '" ' + currentQuote.author
+                  )
+                }
               >
-                <i className="fa fa-twitter"/>
+                <i className="fa fa-twitter" /><span className="visually-hidden">Tweet this quote.</span>
               </a>
             </div>
 
-            <div className="col-md-10 text-right button">
+            <div className="col-md-11 button">
               <button
-                className="btn btn-info"
+                className="btn btn-primary"
                 id="new-quote"
                 onClick={getNewQuote}
               >
@@ -58,22 +64,21 @@ const App = () => {
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
-}
+};
 
 const Quote = (props) => {
   return (
-    <div>
-      <div className="quote-text">
-        <i className="fa fa-quote-left"/>
+    <figure>
+      <blockquote className="quote-text" >
+        <i className="fa fa-quote-left" />
         <span id="text">{props.currentQuote.quote}</span>
-        <i className="fa fa-quote-right"/>
-      </div>
-      <div className="quote-author">
-        <span id="author">- {props.currentQuote.author}</span>
-      </div>
-    </div>
+        <i className="fa fa-quote-right" />
+      </blockquote>
+      <figcaption id="author">- {props.currentQuote.author}</figcaption>
+
+    </figure>
   );
 };
 
